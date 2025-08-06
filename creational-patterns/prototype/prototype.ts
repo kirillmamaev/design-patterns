f5027eec8efc30a0aed1cb50c51ff95bfe8e4940
Kirill Mamaev <kirill.mamaev@mable.com.au>
2025-08-07 02:40:59 +1000

    Update Prototype example

--- creational-patterns/prototype/prototype.ts
+++ creational-patterns/prototype/prototype.ts
@@ -7,37 +7,44 @@ interface Coordinates {
   y: number;
 }
 
-class Dot implements Prototype {
-  protected x: number = 0;
-  protected y: number = 0;
-  protected _color: string = 'white';
-  protected _name: string = '';
+interface ShapePrototype extends Prototype {
+  moveTo(newCoordinates: Coordinates): void;
+  draw(): void;
+  clone: (newName: string) => ShapePrototype;
+}
 
-  constructor(name: string, coordinates: Coordinates = { x: 0, y: 0 }, color: string = 'white') {
-    this._name = name;
-    this.x = coordinates.x;
-    this.y = coordinates.y;
-    this._color = color;
-  }
+class Dot implements ShapePrototype {
+  protected name: string;
+  protected coordinates: Coordinates;
+  protected color: string;
 
-  clone(newName: string): Dot {
-    return new Dot(newName, { x: this.x, y: this.y }, this._color);
+  constructor(
+    name: string,
+    coordinates: Coordinates = { x: 0, y: 0 },
+    color: string = 'white'
+  ) {
+    this.name = name;
+    this.coordinates = { ...coordinates };
+    this.color = color;
   }
 
   moveTo(newCoordinates: Coordinates): void {
-    this.x = newCoordinates.x;
-    this.y = newCoordinates.y;
-    console.log(`Moved ${this._name} to new coordinates: (${this.x}, ${this.y})`);
+    this.coordinates = { ...newCoordinates };
+    console.log(`Moved ${this.name} to new coordinates: (${this.coordinates.x}, ${this.coordinates.y})`);
   }
 
   draw(): void {
-    console.log(`Drawing a dot "${this._name}" at (${this.x}, ${this.y}), color ${this._color}.`);
+    console.log(`Drawing a dot "${this.name}" at (${this.coordinates.x}, ${this.coordinates.y}), color ${this.color}.`);
+  }
+
+  clone(newName: string): Dot {
+    return new Dot(newName, this.coordinates, this.color);
   }
 }
 
 class Rectangle extends Dot {
-  private width: number = 0;
-  private height: number = 0;
+  private width: number;
+  private height: number;
 
   constructor(
     name: string,
@@ -51,42 +58,42 @@ class Rectangle extends Dot {
     this.height = height;
   }
 
-  clone(newName: string): Rectangle {
-    return new Rectangle(newName, { x: this.x, y: this.y }, this._color, this.width, this.height);
+  draw(): void {
+    console.log(`Drawing a rectangle "${this.name}" at (${this.coordinates.x}, ${this.coordinates.y}), width ${this.width}, height ${this.height}, color ${this.color}.`);
   }
 
-  draw(): void {
-    console.log(
-      `Drawing a rectangle "${this._name}" at (${this.x}, ${this.y}), width ${this.width}, height ${this.height}, color ${this._color}.`,
-    );
+  clone(newName: string): Rectangle {
+    return new Rectangle(newName, this.coordinates, this.color, this.width, this.height);
   }
 }
 
 class Circle extends Dot {
-  private radius: number = 0;
+  private radius: number;
 
-  constructor(name: string, coordinates: Coordinates = { x: 0, y: 0 }, color: string = 'white', radius: number = 0) {
+  constructor(
+    name: string,
+     coordinates: Coordinates = { x: 0, y: 0 },
+      color: string = 'white',
+       radius: number = 0) {
     super(name, coordinates, color);
     this.radius = radius;
   }
 
-  clone(newName: string): Circle {
-    return new Circle(newName, { x: this.x, y: this.y }, this._color, this.radius);
+  draw(): void {
+    console.log(`Drawing a circle "${this.name}" at (${this.coordinates.x}, ${this.coordinates.y}), radius ${this.radius}, color ${this.color}.`);
   }
 
-  draw(): void {
-    console.log(
-      `Drawing a circle "${this._name}" at (${this.x}, ${this.y}), radius ${this.radius}, color ${this._color}.`,
-    );
+  clone(newName: string): Circle {
+    return new Circle(newName, this.coordinates, this.color, this.radius);
   }
 }
 
-const Dot1 = new Dot('Dot 01', { x: 10, y: 20 }, 'red');
-const Dot2 = Dot1.clone('Dot 02');
-const Rectangle1 = new Rectangle('Rectangle 01', { x: 30, y: 40 }, 'blue', 100, 50);
-const Rectangle2 = Rectangle1.clone('Rectangle 02');
-const Circle1 = new Circle('Circle 01', { x: 50, y: 60 }, 'green', 25);
-const Circle2 = Circle1.clone('Circle 02');
+const Dot1 = new Dot('Dot 1', { x: 10, y: 20 }, 'red');
+const Dot2 = Dot1.clone('Dot 2');
+const Rectangle1 = new Rectangle('Rectangle 1', { x: 30, y: 40 }, 'blue', 100, 50);
+const Rectangle2 = Rectangle1.clone('Rectangle 2');
+const Circle1 = new Circle('Circle 1', { x: 50, y: 60 }, 'green', 25);
+const Circle2 = Circle1.clone('Circle 2');
 
 Dot1.draw();
 Rectangle1.draw();
@@ -106,15 +113,15 @@ Circle2.draw();
 // > node prototype.ts
 
 // Output:
-// Drawing a dot "Dot 01" at (10, 20), color red.
-// Drawing a rectangle "Rectangle 01" at (30, 40), width 100, height 50, color blue.
-// Drawing a circle "Circle 01" at (50, 60), radius 25, color green.
-// Moved Dot 01 to new coordinates: (110, 120)
-// Moved Rectangle 01 to new coordinates: (230, 240)
-// Moved Circle 01 to new coordinates: (350, 360)
-// Drawing a dot "Dot 01" at (110, 120), color red.
-// Drawing a dot "Dot 02" at (10, 20), color red.
-// Drawing a rectangle "Rectangle 01" at (230, 240), width 100, height 50, color blue.
-// Drawing a rectangle "Rectangle 02" at (30, 40), width 100, height 50, color blue.
-// Drawing a circle "Circle 01" at (350, 360), radius 25, color green.
-// Drawing a circle "Circle 02" at (50, 60), radius 25, color green.
+// Drawing a dot "Dot 1" at (10, 20), color red.
+// Drawing a rectangle "Rectangle 1" at (30, 40), width 100, height 50, color blue.
+// Drawing a circle "Circle 1" at (50, 60), radius 25, color green.
+// Moved Dot 1 to new coordinates: (110, 120)
+// Moved Rectangle 1 to new coordinates: (230, 240)
+// Moved Circle 1 to new coordinates: (350, 360)
+// Drawing a dot "Dot 1" at (110, 120), color red.
+// Drawing a dot "Dot 2" at (10, 20), color red.
+// Drawing a rectangle "Rectangle 1" at (230, 240), width 100, height 50, color blue.
+// Drawing a rectangle "Rectangle 2" at (30, 40), width 100, height 50, color blue.
+// Drawing a circle "Circle 1" at (350, 360), radius 25, color green.
+// Drawing a circle "Circle 2" at (50, 60), radius 25, color green.
